b17604bc6b4cb515ae5a7dff3c721756ed0d330d
Jason Arnold <jason.arnold.00@gmail.com>
2016-10-18 22:34:59 -0400

    add restaurant edit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -23,11 +23,22 @@ class RestaurantsController < ApplicationController
     erb :'restaurants/show'
   end
 
-  post '/restaurants/:id' do
+  post '/restaurants/:id/adduser' do
       restaurant = Restaurant.find(params[:id])
       user = User.find_by(name: params[:user])
       restaurant.users << user
   redirect "/restaurants/#{restaurant[:id]}"
+  end
+
+  get '/restaurants/:id/edit' do
+    @restaurant = Restaurant.find(params[:id])
+    erb :'restaurants/edit'
+  end
+
+  post '/restaurants/:id' do
+    restaurant = Restaurant.find(params[:id])
+    restaurant.update(params[:restaurant])
+    redirect "/restaurants/#{restaurant[:id]}"
   end
 
 end
